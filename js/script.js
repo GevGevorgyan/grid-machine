7ceff1605f3da7e6502853f4a91e65da9c120263
GevorgGevorgyan <gevorik86@gmail.com>
2017-04-04 00:08:59 +0400

    new branch created and files up to date

--- js/script.js
+++ js/script.js
@@ -1,19 +1,45 @@
-function grid() {
-    document.getElementById('divTable').innerHTML = "";
-    var inputValue = document.getElementById('gridInput').value;
-    var value = parseInt(inputValue);
-    var gridTable = document.createElement('table');
-    for (var i = 0; i < inputValue; i++) {
-        var tr = document.createElement('tr');
-        for (var j = 0; j < inputValue; j++) {
-            var td = document.createElement('td');
-            tr.appendChild(td);
-        }
-        gridTable.appendChild(tr);
+'use strict';
+(function IIFE() {
+    var board = {
+        grid: null,
+        value: null,
+        button: null
+    };
+
+    function init() {
+        board.button = document.getElementById('boardButton');
+        board.grid = document.getElementById('inputGrid');
+        bindEvents();
     }
-    document.getElementById('divTable').appendChild(gridTable);
-    if (isNaN(value)) {
-        alert('please type number, instead of string');
+
+    function bindEvents() {
+        board.button.addEventListener("click", function (event) {
+            event.preventDefault();
+            draw();
+        });
     }
-    event.preventDefault(grid);
+function clean() {
+    document.getElementById("gridDraw").innerHTML = "";
+    board.grid.value = "";
 }
+    function draw(event) {
+        board.value="";
+        board.value = document.getElementById('inputGrid').value;
+        var num = parseInt(board.value);
+        if (isNaN(num)) {
+            alert("Please type number ,instead of string")
+        }
+        var cont = document.getElementById("gridDraw");
+       clean();
+        for (var i = 0; i < board.value; i++) {
+            var grid = document.createElement("div");
+            cont.appendChild(grid);
+            grid.className = "container";
+            for (var j = 0; j < board.value; j++) {
+                grid.innerHTML += "<div class='inLine'></div>"
+            }
+        }
+
+    }
+    init();
+}());
