bb5ba4f05d87f6ec4e341a867fb2004c20636664
GevorgGevorgyan <gevorik86@gmail.com>
2017-04-07 23:53:43 +0400

    add colorized function

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@
     var board = {
         grid: null,
         form: null,
-        draw: null
+        draw: null,
+        colorButton: null,
+        containerCount: 1,
+        inLineCount: 0
     };
 
     function init() {
         board.grid = document.getElementById('inputGrid');
         board.form = document.getElementById('drawForm');
         board.draw = document.getElementById('gridDraw');
-
+        board.inlineColor = document.getElementsByClassName('inLine');
+        board.colorButton = document.createElement('input');
         bindEvents();
     }
 
@@ -20,11 +24,44 @@
         } else if (board.form.attachEvent) {
             board.form.attachEvent('submit', draw);
         }
+        board.colorButton.addEventListener('click', colorized)
+    }
+
+    function randomColor() {
+        var red = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        var blue = Math.floor(Math.random() * 256);
+        var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        return color;
+    }
+
+    function colorized() {
+        var inlineColor = document.getElementsByClassName('inLine');
+        var containerColor = document.getElementsByClassName('container');
+        if (board.containerCount > containerColor.length) {
+            return;
+        }
+        if (board.inLineCount < inlineColor.length) {
+            inlineColor[board.inLineCount].style.backgroundColor = randomColor();
+            board.inLineCount += containerColor.length;
+            setTimeout(colorized, 500);
+            if (board.inLineCount >= inlineColor.length) {
+                board.inLineCount = board.containerCount;
+                board.containerCount++;
+            }
+        }
+    }
+
+    function coloredButton() {
+        board.form.appendChild(board.colorButton);
+        board.colorButton.type = 'button';
+        board.colorButton.value = 'colorized';
     }
 
     function clean() {
         board.draw.innerHTML = '';
         board.grid.value = '';
+
     }
     
     function randomColor(){
@@ -50,7 +87,7 @@
                 grid.innerHTML += "<div class='inLine'></div>"
             }
         }
-
+        coloredButton();
     }
 
     if (window.addEventListener) {
